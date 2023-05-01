535f7b5085a30c01bb9a772a2873420bbb8d272c
kwamebrowne <theeecryptoking@gmail.com>
2023-05-01 04:24:35 -0500

    leaving and coming back

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  return (
+  
+  // const colorElements = colors.map((color) => {
+  //   return <ColorItem key={color} color={color} />;
+  // });
+const idk = technologies.map((tech) => {return <span key={tech}>{tech}</span>
+})
+
+return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {/* <span>{technologies[0]}</span>
+        <span>{technologies[1]}</span>
+        <span>{technologies[3] || technologies === '' }</span> */}
+        {idk}
+
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,17 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  
+  const renderP = ProjectItem.map ((projects) => {return <h1 key={projects.id}>{ProjectItem}</h1>}); 
+
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{
+        /* render ProjectItem components here */
+       renderP
+      }</div>
     </div>
   );
 }
